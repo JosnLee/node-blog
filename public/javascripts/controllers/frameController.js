3e2146bb3e835f9d3c343892ada97516145c321e
Jerry <479182117@qq.com>
2016-02-01 11:18:47 +0800

    add  other whise

--- public/javascripts/controllers/frameController.js
+++ public/javascripts/controllers/frameController.js
@@ -1,7 +1,7 @@
 angular.module('nodeBlog', ['ui.bootstrap','ui.router']).config(function($stateProvider, $urlRouterProvider) {
     //
     // For any unmatched url, redirect to /state1
-    //
+    $urlRouterProvider.otherwise("/home");
     // Now set up the states
     $stateProvider
         .state('home', {
